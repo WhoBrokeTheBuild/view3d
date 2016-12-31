c274a80db7570c1a6c4ec49d792c2bf55ea25e84
Stephen Lane-Walsh <slanewalsh@mywebgrocer.com>
2016-12-31 02:32:54 -0500

    not what I wanted, but cool

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <GL/freeglut.h>
 #include <glmm/glmm.h>
 #include <stdio.h>
+#include <time.h>
 
 #include <camera.h>
 #include <debug.h>
@@ -17,7 +18,13 @@ GLuint g_vao;
 camera_t g_cam;
 mat4x4_t g_model;
 
-GLint g_mvp_loc;
+GLint g_mvp_loc, g_color_loc;
+
+float g_rot = 0.0f;
+vec3f_t g_rot_axis = { 0.0f, 0.0f, 1.0f };
+
+vec3f_t g_color;
+unsigned int g_color_timer = 0;
 
 void update()
 {
@@ -27,28 +34,39 @@ void render()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    LOG_INFO("Rendering");
-
     camera_update(&g_cam);
+
+    mat4x4_init(g_model, 1.0f);
+    mat4x4_rotate(g_model, g_rot, g_rot_axis);
+
+    if (g_color_timer <= 0)
+    {
+        g_color[0] = (float)(rand() % 255) / 255.0f;
+        g_color[1] = (float)(rand() % 255) / 255.0f;
+        g_color[2] = (float)(rand() % 255) / 255.0f;
+        g_color_timer = 5;
+    }
+    --g_color_timer;
+
+    g_rot += 0.1f;
 
     mat4x4_t mvp;
     mat4x4_mul(mvp, g_cam.proj, g_cam.view);
     mat4x4_mul(mvp, mvp, g_model);
 
-    printf("MVP: \n");
-    mat4x4_print(mvp);
-    printf("\n");
-    
     glUseProgram(g_program);
     glUniformMatrix4fv(g_mvp_loc, 1, GL_FALSE, (GLfloat *)mvp);
+    glUniform3fv(g_color_loc, 1, (GLfloat *)g_color);
     glBindVertexArray(g_vao);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawArrays(GL_TRIANGLES, 0, 24);
 
     glutSwapBuffers();
 }
 
 int main(int argc, char **argv)
 {
+    srand(time(0));
+
     glutInit(&argc, argv);
     glutInitContextVersion(3, 3);
     glutInitContextFlags(GLUT_FORWARD_COMPATIBLE | GLUT_DEBUG);
@@ -74,6 +92,7 @@ int main(int argc, char **argv)
     printf("Running GLEW Version: %d.%d.%d\n", GLEW_VERSION_MAJOR, GLEW_VERSION_MINOR, GLEW_VERSION_MICRO);
 
     glutDisplayFunc(&render);
+    glutIdleFunc(&render);
 
     camera_init(&g_cam, WIDTH, HEIGHT, 0.1f, 1000.0f, GLMM_RAD(45.0f));
 
@@ -84,16 +103,44 @@ int main(int argc, char **argv)
 
     camera_update(&g_cam);
     camera_print(&g_cam);
-    printf("Model:\n");
-
-    mat4x4_init(g_model, 1.0f);
-    mat4x4_print(g_model); 
-    printf("\n");
 
     float points[] = {
-       -1.0f, -1.0f, 0.0f,
-       1.0f, -1.0f, 0.0f,
-       0.0f,  1.0f, 0.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f,-1.0f, 1.0f,
+		-1.0f, 1.0f, 1.0f, 
+		1.0f, 1.0f,-1.0f, 
+		-1.0f,-1.0f,-1.0f,
+		-1.0f, 1.0f,-1.0f,
+		1.0f,-1.0f, 1.0f,
+		-1.0f,-1.0f,-1.0f,
+		1.0f,-1.0f,-1.0f,
+		1.0f, 1.0f,-1.0f,
+		1.0f,-1.0f,-1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f,-1.0f,
+		1.0f,-1.0f, 1.0f,
+		-1.0f,-1.0f, 1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+		-1.0f,-1.0f, 1.0f,
+		1.0f,-1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f,-1.0f,-1.0f,
+		1.0f, 1.0f,-1.0f,
+		1.0f,-1.0f,-1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f,-1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f,-1.0f,
+		-1.0f, 1.0f,-1.0f,
+		1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+		1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f, 1.0f,
+		1.0f,-1.0f, 1.0f
     };
 
     GLuint vbo = 0;
@@ -116,6 +163,7 @@ int main(int argc, char **argv)
     g_program = shader_program_load(shaders);
 
     g_mvp_loc = glGetUniformLocation(g_program, "mat_mvp");
+    g_color_loc = glGetUniformLocation(g_program, "f_color");
 
     glutMainLoop();
 
